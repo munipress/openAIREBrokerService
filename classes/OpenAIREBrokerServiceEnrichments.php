9868cc01ecf446e93fa798be84baf9c15b222f95
munipress <gomola@press.muni.cz>
2025-06-20 09:50:37 +0200

    fix the problem with another IDs mixed in JSON stream

--- classes/OpenAIREBrokerServiceEnrichments.php
+++ classes/OpenAIREBrokerServiceEnrichments.php
@@ -135,13 +135,24 @@ class OpenAIREBrokerServiceEnrichments {
                 if ($item['topic'] != $topic) {
                     continue;
                 }
-
-
+;
+                $oaiTest = strrpos($item['originalId'], 'oai');
+                
+                if ($oaiTest === false) {
+                    continue;
+                }
+                
                 $pos = strrpos($item['originalId'], '/');
+                
                 if (!$pos) {
                     continue;
                 }
+                
                 $jsonSubmissionId = substr($item['originalId'], $pos + 1);
+                
+                if(!preg_match('/^-?\d+$/', $jsonSubmissionId)){
+                    continue;
+                }
                 $idToCompare = $pos !== false ? $jsonSubmissionId : $item['originalId'];
 
                 if ($submissionId) {
